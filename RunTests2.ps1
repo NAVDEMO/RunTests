6a537610a247e1bc997d887a0d6576b44ac2a3c5
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-03-03 06:30:42 -0800

    Use TestFunctions instead of TestRunner

--- RunTests2.ps1
+++ RunTests2.ps1
@@ -2,13 +2,31 @@
 $newtonSoftDllPath = (Join-Path $PSScriptRoot "Test Assemblies\NewtonSoft.json.dll")
 
 $serviceUrl = "http://fkdev/NAV/cs"
+$clientServicesCredentialType = "NavUserPassword"
 $credential = New-Object pscredential 'admin', (ConvertTo-SecureString -String 'P@ssword1' -AsPlainText -Force)
 
 $XUnitResultFileName = "c:\temp\results.xml"
-    
-. (Join-Path $PSScriptRoot "PsTestRunner.ps1") -newtonSoftDllPath $newtonSoftDllPath `
-                                               -clientDllPath $clientDllPath `
-                                               -serviceUrl $serviceUrl `
-                                               -credential $credential `                                               -detailed `                                               -AzureDevOps error `                                               -XUnitResultFileName $XUnitResultFileName
+
+$testSuite = "DEFAULT"
+$testCodeunit = "*"
+$testFunction = "FinanceChargeMemoAndApply"
+$disableSslVerification = $false
+
+. (Join-Path $PSScriptRoot "PsTestFunctions.ps1") -newtonSoftDllPath $newtonSoftDllPath -clientDllPath $clientDllPath -clientContextScriptPath $ClientContextPath
+
+try {    if ($disableSslVerification) {        Disable-SslVerification    }        $clientContext = New-ClientContext -serviceUrl $serviceUrl `                                       -auth $clientServicesCredentialType `                                       -credential $credential
+    $tests = Get-Tests -clientContext $clientContext | ConvertFrom-Json
+
+   # Run-Tests -clientContext $clientContext `
+   #           -TestSuite $testSuite `
+   #           -TestCodeunit $testCodeunit `
+   #           -TestFunction $testFunction `
+   #           -XUnitResultFileName $XUnitResultFileName `
+   #           -AzureDevOps $AzureDevOps `
+   #           -detailed:$detailed
+}
+finally {
+    if ($disableSslVerification) {        Enable-SslVerification    }    Remove-ClientContext -clientContext $clientContext
+}
 
 & notepad.exe $XUnitResultFileName
